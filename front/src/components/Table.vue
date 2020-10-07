e8a59109bf212a4730a86d8d1ad5fdffdb373850
Cyrille Nicaise <cyrille.ni1@gmail.com>
2020-10-08 00:10:33 +0200

    basic component structure

---
+++ front/src/components/Table.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!--Need to implement a grid system here based on the -->
+  </div>
+</template>
+
+<script>
+export default {
+    data: {
+        atoms: {}
+    },
+  methods: {
+    fetchData() {
+      fetch("THEAPIROUTETOFETCH", { //Fetch returns a promise
+        method: GET,
+        headers: {
+          "theAPIKEY ?": "KEY"
+        },
+      }).then((response) => { //Here is the "promise" returned by the fetch JS API 
+        if (response.ok) {
+          return response.json; //Function wich return another "promise" Maybe don't need to transform the json into json... but have to test with the API
+        } else { 
+            alert("Something went wrong : " + response.status + ' ' + response.statusText);
+        }
+      }).then(response => { () //.json promise retrieved here
+          this.data.atoms = response //Need to check the response.body instead but not sure...
+      }).catch(err => {
+          console.log(err)
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
